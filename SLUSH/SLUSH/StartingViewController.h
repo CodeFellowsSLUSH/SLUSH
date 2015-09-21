39a7940f270412839c0aa328227c4ea03eaf1871
Mark Lin <m4rklin@comcast.net>
2015-09-21 13:06:18 -0700

    making Xcode happy

---
+++ SLUSH/SLUSH/StartingViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartingViewController.h
+//  SLUSH
+//
+//  Created by Mark Lin on 9/21/15.
+//  Copyright © 2015 Benjamin Laddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartingViewController : UITabBarController
+
+@end
---
+++ SLUSH/SLUSH/StartingViewController.m
@@ -0,0 +1,38 @@
+//
+//  StartingViewController.m
+//  SLUSH
+//
+//  Created by Mark Lin on 9/21/15.
+//  Copyright © 2015 Benjamin Laddin. All rights reserved.
+//
+
+#import "StartingViewController.h"
+
+@interface StartingViewController ()
+
+@end
+
+@implementation StartingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
