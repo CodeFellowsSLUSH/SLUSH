a32cf046dc1963211525252cf7036e69c08590df
Benjamin Laddin <benjamin.laddin@gmail.com>
2015-09-22 13:09:12 -0700

    labels can be altered, cells and labels are dynamic

---
+++ SLUSH/SLUSH/BrowseListingCellsTableViewCell.m
@@ -0,0 +1,15 @@
+//
+//  BrowseListingCellsTableViewCell.m
+//  SLUSH
+//
+//  Created by Benjamin Laddin on 9/22/15.
+//  Copyright © 2015 Chris Budro, Benjamin Laddin, Stephen Lardieri, and Mark Lin. All rights reserved.
+//
+
+#import "BrowseListingCellsTableViewCell.h"
+
+
+@implementation BrowseListingCellsTableViewCell
+
+@end
+
