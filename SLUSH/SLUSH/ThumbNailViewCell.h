c2750fa75a5d1ad24b442b2853557aef610cc920
Mark Lin <m4rklin@comcast.net>
2015-09-25 00:44:12 -0700

    image picker done

---
+++ SLUSH/SLUSH/ThumbNailViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ThumbNailViewCell.h
+//  SLUSH
+//
+//  Created by Mark Lin on 9/24/15.
+//  Copyright © 2015 Chris Budro, Benjamin Laddin, Stephen Lardieri, and Mark Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThumbNailViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewCell;
+
+@end
